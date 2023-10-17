13b502bb075e0c906ac25e5453811636bed9c18e
Archei1 <125820933+Archei1@users.noreply.github.com>
2023-10-17 09:39:28 +0300

    Create js.js

---
+++ js.js
@@ -0,0 +1,9 @@
+<script>
+        function goToPortfolio() {
+            window.location.href = "portfolio.html";
+        }
+
+        function goToServices() {
+            window.location.href = "Service.html";
+        }
+    </script>
